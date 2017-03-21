d9409f9ba4b654159374442bf838f99fac139817
Hannah Schuster <hschuster9@gmail.com>
2017-03-21 14:19:04 -0400

    before connected

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const app = express()
 
 const Note = mongoose.model("Note")
 
-
-
-
 app.set("port", process.env.PORT || 9111)
 app.set('view engine', 'hbs')
 app.engine(".hbs", hbs({
@@ -23,7 +20,10 @@ app.use("/assets", express.static("public"))
 app.use(parser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
-  res.render("notes")
+  console.log("CONNECTED")
 })
 
-app.get()
+
+app.listen(app.get("port"), function(){
+  console.log("It's aliiive!");
+});
