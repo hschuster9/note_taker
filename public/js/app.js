436d8ae54c246963001cd7c4628f3bdc3415fd9b
Hannah Schuster <hschuster9@gmail.com>
2017-03-21 14:47:37 -0400

    initial setup for angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,32 @@
 angular
+  .module("notesApp", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory("NoteFactory", [
+    "$resource",
+    NoteFactoryFunction
+  ])
+  .controller('NoteIndexController', [
+    "NoteFactory",
+    "$state",
+    NoteIndexControllerFunction
+  ])
+  .controller('NoteShowController', [
+    "NoteFactory",
+    "$state",
+    "$stateParams",
+    NoteShowControllerFunction
+  ])
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/js/ng-views/welcome.html"
+    })
+  }
