ae47b1de424a1276f2266537e2f716c33488ddc6
Hannah Schuster <hschuster9@gmail.com>
2017-03-21 14:09:20 -0400

    trying to connect to localhost

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const parser = require('body-parser')
+const mongoose = require('./db/connection')
+
+const app = express()
+
+const Note = mongoose.model("Note")
+
+
+
+
+app.set("port", process.env.PORT || 9111)
+app.set('view engine', 'hbs')
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}))
+
+app.use("/assets", express.static("public"))
+app.use(parser.urlencoded({extended: true}))
+
+app.get('/', function(req, res){
+  res.render("notes")
+})
+
+app.get()
